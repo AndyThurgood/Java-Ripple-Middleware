db66bcad545d3375ade160e729d4d4ec1d06b3fc
Josh.Hilland <josh.hilland@answerdigital.com>
2016-08-05 10:23:48 +0100

    Moved security config from API to the ripple-security module

--- ripple-security/src/main/java/org/rippleosi/config/SecurityConfig.java
+++ ripple-security/src/main/java/org/rippleosi/config/SecurityConfig.java
@@ -13,7 +13,7 @@
  *    See the License for the specific language governing permissions and
  *    limitations under the License.
  */
-package org.rippleosi.config.security;
+package org.rippleosi.config;
 
 import org.pac4j.core.config.Config;
 import org.rippleosi.security.interceptor.authentication.RequiresAuthenticationInterceptor;
